902e0379d76e6108417c14dbef5050ac52e18722
Artem <artskadin@gmail.com>
2020-12-30 20:01:07 +0700

    Update index.js

--- public_html/result4/index.js
+++ public_html/result4/index.js
@@ -1,1 +1,21 @@
-console.log('hello')
+const { Server } = require('http');
+const { createReadStream: cRS, writeFileSync: wFS } = require('fs');
+const finalhandler = require('finalhandler');
+const serve = require('serve-static')('.');
+
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};
+const s = Server((req, res) => {
+    if (req.url.startsWith('/result4/')) {
+        const params = req.url.substring(1 + req.url.indexOf('?'));
+        res.write(params);           
+    } else {
+        // return finalhandler(req, res)();
+        return serve(req, res, finalhandler(req, res));
+    }
+    res.end();
+});
+s.listen(4321);
